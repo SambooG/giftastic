6b7af7de2b9e7a0acc0fc0019d7945b9650f2e6b
Samantha Gaiser <sgaiser3@gmail.com>
2019-11-07 18:44:47 -0500

    starting the ajax function

---
+++ giftastic.js
@@ -0,0 +1,10 @@
+const topics = ("Rick and Morty", "Modern Family", "family Guy", "The Sporanos", "Sex and the City", "Insecure", "skiing", "pets");
+var url = "https//api.giphy.com/v1/" + "1JgZ7zJzRQyi6ykHxyuaaRypiysOaUQm";
+
+$.ajax({
+    url: url,
+    method: "GET",
+    success: function(){
+        console.log()
+    }
+});
